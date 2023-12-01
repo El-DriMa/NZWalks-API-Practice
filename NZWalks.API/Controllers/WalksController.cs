529c784dcac0d45e235118bea90c9445b04d7930
Amna Hodžić <ahodzic649@outlook.com>
2023-12-02 00:16:08 +0100

    interface for walks

---
+++ NZWalks.API/Controllers/WalksController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Models.DTO;
+using NZWalks.API.Repositories;
+
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WalksController : ControllerBase
+    {
+        private readonly NZWalksDbContext dbConext;
+        private readonly IWalksRepository walksRepository;
+
+        public WalksController(NZWalksDbContext dbContext,IWalksRepository walkRepository)
+        {
+            this.dbConext = dbContext;
+            this.walksRepository = walkRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var walksDomainModel = await walksRepository.GetAllAsync();
+
+            var walksDTO = new List<WalkDTO>();
+            foreach(var walkDomain in walksDomainModel)
+            {
+                walksDTO.Add(new WalkDTO()
+                {
+                    Id = walkDomain.Id,
+                    Name = walkDomain.Name,
+                    Description = walkDomain.Description,
+                    LengthInKm=walkDomain.LengthInKm,
+                    WalkImageUrl = walkDomain.WalkImageUrl,
+                    DifficultyId = walkDomain.DifficultyId,
+                    RegionId = walkDomain.RegionId,
+
+                }); 
+            }
+
+            return Ok(walksDTO);
+        }
+    }
+}
---
+++ NZWalks.API/Repositories/SQLWalkRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Repositories
+{
+    public class SQLWalkRepository : IWalksRepository
+    {
+        private readonly NZWalksDbContext dbContext;
+
+        public SQLWalkRepository(NZWalksDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public async Task<List<Walk>> GetAllAsync()
+        {
+            return await dbContext.Walks.ToListAsync();
+        }
+    }
+}
