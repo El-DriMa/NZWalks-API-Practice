2db38e7c07b7bf0297ff601a9d7f3d23bf4c756c
Amna Hodžić <ahodzic649@outlook.com>
2023-12-28 17:15:16 +0100

    difficulty getAll fixed

--- NZWalks.API/Models/DTO/DifficultyDTO.cs
+++ NZWalks.API/Models/DTO/DifficultyDTO.cs
@@ -5,9 +5,7 @@ namespace NZWalks.API.Models.DTO
     public class DifficultyDTO
     {
         public Guid Id { get; set; }
-        public string Code { get; set; }
         public string Name { get; set; }
-        public string? ImageUrl { get; set; }
-
+       
     }
 }
